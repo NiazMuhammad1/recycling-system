c1bcca9faaa8f75b5c08eeb18c3b5a967bae74f4
NiazMuhammad1 <38530379+NiazMuhammad1@users.noreply.github.com>
2026-01-19 22:02:33 +0500

    m

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body class="login-page">
+
+    @yield('content')
+
+</body>
+</html>
